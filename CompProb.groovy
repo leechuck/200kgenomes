2f35eec29cc50edd9bd69f91bfd9b7c86e5ec29c
Robert Hoehndorf <leechuck@leechuck.de>
2020-11-22 12:22:24 +0300

    replace numerical id with UKB id

--- CompProb.groovy
+++ CompProb.groovy
@@ -31,9 +31,17 @@ def homs = [:].withDefault { 0 } // keeps max for current gene only; index -> ma
 def current = ""
 //PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter(args[2])))
 EPSILON = 0.001
+def start = true
+def index2id = [:]
 withPool(16) {
     new java.util.zip.GZIPInputStream(new FileInputStream(args[1])).splitEachLine("\t") { line ->
-    if (! line[0].startsWith("#")) {
+	if (start && line[0].startsWith("#CHROM")) {
+	    start = false
+	    line[9..-1].eachWithIndex { id, idx ->
+		index2id[idx] = id
+	    }
+	}
+	if (! line[0].startsWith("#")) {
 	    def chr = line[0]
 	    def pos = new Integer(line[1])
 	    def alt = line[4]
@@ -48,11 +56,11 @@ withPool(16) {
 		if (l.size()>0) {
 		    l.sort()
 		    l2.sort()
-		    hets.each { k, v ->
+		    hets.each { k, v -> // hets include the homs, so we just iterate through the hets
 			def hh = homs[k]
 			def q = Descriptive.quantileInverse(l, v - EPSILON) // strictly LESS
 			def hq = Descriptive.quantileInverse(l2, hh - EPSILON) // strictly LESS
-			println "$gene\t$k\t$v\t$q\t$hh\t$hq"
+			println "$gene\t${index2id[k]}\t$v\t$q\t$hh\t$hq"
 		    }
 		}
 		// reset to new gene
